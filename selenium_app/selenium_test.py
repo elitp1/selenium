dfcd1066f64beff919a92431c3bedf9a73666197
Hilit <hilit@seebo.com>
2025-08-11 12:24:06 +0300

    asasasa

--- selenium_app/selenium_test.py
+++ selenium_app/selenium_test.py
@@ -3,6 +3,7 @@ import allure
 import time
 from playwrightobject import PlaywrightObject
 from selenium_actions import SeleniumActions, LoginSeleniumActions
+from selenium_app.playwrite_actions import PlaywrightActions
 from selenium_object import SeleniumObject
 
 
@@ -11,19 +12,20 @@ from selenium_object import SeleniumObject
 @allure.story("story level")
 @allure.title("Test Create New Account on Facebook1")
 @allure.tag("tag1")
-def ytest_create_new_account1():
-    with PlaywrightObject("https://facebook.com") as playwrite_obj:
-        playwrite_obj.click_btn_by_txt("Create new account")
-        playwrite_obj.fill_input_by_name("firstname", "Hilit")
-        playwrite_obj.fill_input_by_name("lastname", "prizant")
-        playwrite_obj.select_option_from_drop_down_by_name("birthday_month", "Oct")
-        playwrite_obj.select_option_from_drop_down_by_name("birthday_day", "3")
-        playwrite_obj.select_option_from_drop_down_by_name("birthday_year", "1973")
-        playwrite_obj.select_option_from_radio_btn_by_value("sex", "2")
-        playwrite_obj.fill_input_by_name("reg_email__", "elitp@walla.co.il")
-        print(f'the last name is {playwrite_obj.get_value_of_element_by_name("lastname")}')
-        playwrite_obj.click_btn_by_name("websubmit")
-        time.sleep(3)
+def test_create_new_account1():
+    with PlaywrightObject() as playwright_obj:
+        playwright_obj.navigate_to("https://www.facebook.com/")
+        playwright_actions = PlaywrightActions(playwright_obj.page)
+        playwright_actions.click_btn_by_txt("Create new account")
+        playwright_actions.fill_input_by_name("firstname", "Hilit")
+        playwright_actions.fill_input_by_name("lastname", "prizant")
+        playwright_actions.select_option_from_drop_down_by_name("birthday_month", "Oct")
+        playwright_actions.select_option_from_drop_down_by_name("birthday_day", "3")
+        playwright_actions.select_option_from_drop_down_by_name("birthday_year", "1973")
+        playwright_actions.select_option_from_radio_btn_by_value("sex", "2")
+        playwright_actions.fill_input_by_name("reg_email__", "elitp@walla.co.il")
+        playwright_actions.click_btn_by_name("websubmit")
+        time.sleep(10)
         assert True
 
 
